98168bff3a74e3d7d55d46dffd72d38b724d6012
Sushmithagk21 <sushmithagk2001@gmail.com>
2024-12-25 11:50:27 +0530

    Modiofied  Operators File

--- Operators.js
+++ Operators.js
@@ -4,5 +4,17 @@
  console.log(a++);
  console.log(a);
  console.log(++a);
+
+
+ console.log(10 == '10'); //true
+console.log(10 === '10');//false : Values are matching but not data type
+console.log(10 == 10);//true
+
+// logical && || and !
+console.log(true && true);//true
+console.log(true || false); //true
+console.log(!true);//false
+//Ternary Operator:
+console.log((10 == '10' ? 'Equal by Value': 'Not Equal by Value'));
  
  
