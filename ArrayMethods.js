101b4a33f802ed398d1aaa6b45916556f6980c3a
Sushmithagk21 <sushmithagk2001@gmail.com>
2024-12-26 11:08:43 +0530

    26th Dec

---
+++ ArrayMethods.js
@@ -0,0 +1,18 @@
+//var arr1 = [10,11,22,33,44]
+//console.log(arr1);
+
+//for (var i=0;i<=arr1.length-1;i++) {
+  //  if(arr1[i]%2==0) {
+
+    //console.log(arr1[i]);
+//}
+//}
+
+
+
+var arr2=[10,20,30,40]
+arr2.push(50);
+console.log('After push:', arr2);
+
+arr2.unshift(5);
+console.log('After unshift:',arr2);
