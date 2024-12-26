5db77e3bf61d6540141764a2ec180e5a996a1766
Sushmithagk21 <sushmithagk2001@gmail.com>
2024-12-26 10:32:40 +0530

    Array file

---
+++ Array.js
@@ -0,0 +1,16 @@
+var arr1 = [10,20,30,40,50,60]
+console.log('The length of arr1 array:',arr1.length);
+console.log('Element at 0th index:',arr1[0]);
+console.log('Element at 1st index:',arr1[1]);
+console.log('Element at 2nd index:',arr1[2]);
+console.log('Element at 3rd index:',arr1[3]);
+
+for(var i=0;i<=3;i++) {
+    console.log(`Element at index ${i}`,arr1[i]);
+}
+
+
+for(var i=0;i<=arr1.length-1;i++) {
+    console.log(`Element at index ${i}`,arr1[i]);
+}
+
